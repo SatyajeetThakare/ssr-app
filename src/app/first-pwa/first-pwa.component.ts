a00f235f367884b161958a2d0a0dfbaf959982da
pshamunesh <p.shamunesh@bajajfinserv.in>
2025-02-27 10:53:35 +0530

    PWA Content updated

--- src/app/first-pwa/first-pwa.component.ts
+++ src/app/first-pwa/first-pwa.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 
 @Component({
@@ -9,17 +9,19 @@ import { Component,OnInit } from '@angular/core';
   styleUrl: './first-pwa.component.scss',
 
 })
-export class FirstPwaComponent implements OnInit  {
+export class FirstPwaComponent implements OnInit, OnDestroy {
 
   msg: string = '';
+  currentTime: string = '';
+  private timeInterval!: any;
 
   ngOnInit() {
     this.setGreeting();
+    this.updateTime();
   }
 
   setGreeting() {
     const hours = new Date().getHours();
-
     if (hours < 12) {
       this.msg = 'Good Morning';
     } else if (hours < 18) {
@@ -28,4 +30,15 @@ export class FirstPwaComponent implements OnInit  {
       this.msg = 'Good Evening';
     }
   }
+
+  updateTime() {
+    this.currentTime = new Date().toLocaleTimeString(); // Set initial time
+    this.timeInterval = setInterval(() => {
+      this.currentTime = new Date().toLocaleTimeString();
+    }, 1000); // Update time every second
   }
+
+  ngOnDestroy() {
+    clearInterval(this.timeInterval); // Clear interval when component is destroyed
+  }
+}
